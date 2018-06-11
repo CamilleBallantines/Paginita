2e3920411018cfeda1304b8e8166457da043f67a
Jose Invernon <j.inver93@gmail.com>
2018-06-11 00:12:26 -0400

    contact pills button

--- src/app/components/contacts/contacts.component.ts
+++ src/app/components/contacts/contacts.component.ts
@@ -7,10 +7,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactsComponent implements OnInit {
 
+  public formState : string = 'lessons'; 
+ 
+
   constructor() { }
 
   ngOnInit() {
     window.scrollTo(0, 0);
   }
-
+  
+  private showForm( option: string ){
+    switch (option) {
+      case 'translation':
+        this.formState = 'translation'
+        break;
+      case 'general':
+        this.formState = 'general'
+        break;
+      default:
+        this.formState = 'lessons'
+        break;
+    }
+  }
 }
